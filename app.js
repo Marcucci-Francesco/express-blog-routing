ae4ace8b3a5ca4b0fe0898de9f89725bb404ff3d
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-12-18 12:44:49 +0100

    feat: add array, yours export and import

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+const posts = require('./data/blog');
 const router = require('./routers/posts');
 const express = require('express');
 const app = express();
 const port = 3000;
 
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+})
+
+app.listen(port, () => {
+  console.log(`Sono in ascolto alla porta ${port}`)
+});
+
 app.use('/posts', router);
+
+app.use(express.static('immagini_e_posts'));
---
+++ data/blog.js
@@ -0,0 +1,39 @@
+const posts = [
+  {
+    id: 1,
+    titolo: "Ciambellone",
+    contenuto: "Il ciambellone è uno dei dolci più soffici",
+    immagine: "images/ciambellone.jpeg",
+    tags: ["#ciambellone", "#dolciDaForno", "#colazione"]
+  },
+  {
+    id: 2,
+    titolo: "Cracker alla barbabietola",
+    contenuto: "I cracker alla barbabietola sono uno snack salutare, perfetto per un aperitivo o una merenda leggera",
+    immagine: "images/cracker_barbabietola.jpeg",
+    tags: ["#cracker", "#barbabietola", "#snack"]
+  },
+  {
+    id: 3,
+    titolo: "Pane fritto",
+    contenuto: "Il pane fritto è un piatto semplice e gustoso, perfetto per utilizzare il pane avanzato",
+    immagine: "images/pane_fritto_dolce.jpeg",
+    tags: ["#paneFritto", "#antipasti", "#cucina"]
+  },
+  {
+    id: 4,
+    titolo: "Pasta alla barbabietola",
+    contenuto: "La pasta alla barbabietola è un piatto che unisce la dolcezza e il colore intenso della barbabietola con la tradizione della pasta fresca",
+    immagine: "images/pasta_barbabietola.jpeg",
+    tags: ["#pastaFresca", "#ricetteVegetariane", "#cucinaColorata"]
+  },
+  {
+    id: 5,
+    titolo: "Torta paesana",
+    contenuto: "La torta paesana si caratterizza per la sua semplicità e l’uso di ingredienti poveri ma ricchi di sapore",
+    immagine: "images/torta_paesana.jpeg",
+    tags: ["#dolceLombardo", "#ricetteTradizionali", "#cucinaRustica"]
+  }
+];
+
+module.exports = posts;
